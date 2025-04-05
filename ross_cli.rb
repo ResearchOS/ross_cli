854cffa6a37c7aa4f355176b048f41e51e1cf0c2
[Mitchell Tillman] <[mtillman14@gmail.com]>
2025-04-05 16:54:00 -0500

    New try with different URL?

--- ross_cli.rb
+++ ross_cli.rb
@@ -3,7 +3,7 @@ class RossCli < Formula
 
   desc "Ross CLI tool"
   homepage "https://github.com/ResearchOS/ross_cli"
-  url "https://github.com/ResearchOS/ross_cli/releases/download/v0.1.1.tar.gz"
+  url "https://github.com/ResearchOS/ross_cli/archive/v0.1.1.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
